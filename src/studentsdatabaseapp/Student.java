c6ccef9dd44e071a6f08157a307c654b78b0a8e5
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2025-04-30 16:30:00 +0530

    Initial commit with Java class

---
+++ src/studentsdatabaseapp/Student.java
@@ -0,0 +1,5 @@
+package studentsdatabaseapp;
+
+public class Student {
+
+}
---
+++ src/studentsdatabaseapp/StudentsDataBaseApp.java
@@ -0,0 +1,10 @@
+package studentsdatabaseapp;
+
+public class StudentsDataBaseApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
